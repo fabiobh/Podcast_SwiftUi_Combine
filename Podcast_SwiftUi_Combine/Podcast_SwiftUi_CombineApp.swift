4ba2185aab8418999f5402fb8be793d10534f47c
fabiobh <psxbrasil@gmail.com>
2024-12-07 17:33:49 -0300

    first commit -m "up"

---
+++ Podcast_SwiftUi_Combine/Podcast_SwiftUi_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Podcast_SwiftUi_CombineApp.swift
+//  Podcast_SwiftUi_Combine
+//
+//  Created by FabioCunha on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Podcast_SwiftUi_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()            
+        }
+    }
+}
