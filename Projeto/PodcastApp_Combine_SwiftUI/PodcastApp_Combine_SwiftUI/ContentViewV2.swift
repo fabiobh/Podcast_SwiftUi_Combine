c1b871197e6bf104f59eaaaa031a31a17299e4d3
fabiobh <psxbrasil@gmail.com>
2024-12-08 16:13:10 -0300

    downgrade xcode

---
+++ Projeto/PodcastApp_Combine_SwiftUI/PodcastApp_Combine_SwiftUI/ContentViewV2.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PodcastApp_Combine_SwiftUI
+//
+//  Created by FabioCunha on 08/12/24.
+//
+
+import SwiftUI
+/*
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+*/
---
+++ Projeto/PodcastApp_Combine_SwiftUI/PodcastApp_Combine_SwiftUI/PodcastApp_Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastApp_Combine_SwiftUIApp.swift
+//  PodcastApp_Combine_SwiftUI
+//
+//  Created by FabioCunha on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PodcastApp_Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
